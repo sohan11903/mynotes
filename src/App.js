24bdbbcfa8f9c41224ddce1d80c5e5e6b91a1280
sohan11903 <104675668+sohan11903@users.noreply.github.com>
2024-02-08 18:18:11 +0530

    fial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-
 import './App.css';
-import Header from './header.jsx';
-import Footer from './footer.jsx';
-import CreateNote from './createnote.jsx';
-import Note from './note.jsx';
+import Header from './header';
+import CreateNote from './createnote';
+import Note from './note';
+import Footer from './footer';
+import { useState } from 'react';
 
 function App() {
+  const [addItem,setaddItem]=useState([]);
+
+  const addNote =(note)=>{
+    setaddItem((prevData)=>{
+      return [...prevData,note]
+    })
+  };
+const onDelete = (id) =>{
+  setaddItem((olddata) => 
+  olddata.filter((currdata, indx)=>
+  {
+    return indx !== id;
+  })
+  )
+}
   return (
     <>
-    <Header />
-    <CreateNote />
-    <Note />
-    <Footer />
+      <Header />
+      <CreateNote passNote={addNote}/>
+      <br/>
+      <hr/>
+      <br/>
+      {addItem.map((val,index)=>{ 
+        if(val.title=="" && val.content==""){
+          
+        }
+        else{
+          return(
+            <>
+            <div className='container'>
+            <Note 
+        key={index}
+        id={index}
+        title={val.title}
+        content={val.content}
+        deleteItem={onDelete}
+        />
+
+            </div>
+           
+
+          </>
+        );
+        }
+      
+      })}
+      <Footer />
     </>
   );
 }
--- src/createnote.jsx
+++ src/createnote.jsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import ZoomOutMapRoundedIcon from '@mui/icons-material/ZoomOutMapRounded';
+import ZoomInMapRoundedIcon from '@mui/icons-material/ZoomInMapRounded';
 
-export default function CreateNote() {
-  return (
-    <>
-        <div className='main_note'>
-        <form>
-            <input type='text' placeholder='Title'></input>
-            <textarea rows="" column="" placeholder='Write a note.' ></textarea>
-            <Button><AddIcon className="plus_sign"></AddIcon></Button>
-        </form>
 
-        </div>
-    </>
-  )
+
+function CreateNote(props) {
+const [expand,setExpand] = useState(false);
+
+    const [note, setNote] = useState({
+        title: '',
+        content: '',
+    });
+
+    const InputEvent = (event) => {
+        const { name, value } = event.target;
+        setNote((prevData) => {
+            return {
+                
+                ...prevData,
+                [name]: value,
+            }
+        })
+    };
+
+    const addEvent = () => {
+      
+      props.passNote(note);
+      
+        setNote ({
+          title: '',
+          content: '',
+      });
+
+      
+     
+    }
+
+    const abc = () =>{
+      setExpand(true);
+    }
+const abcd = () =>{
+  setExpand(false);
 }
+    return (
+        <>
+            <div className="main_note">
+                <form action="">
+                 {expand ?
+                  <input type="text"  name='title' onChange={InputEvent} value={note.title} placeholder='Title' autoComplete='off' /> 
+                  : null
+
+                 }   
+                    <textarea cols="" onClick={abc} onDoubleClick={abcd} name='content' onChange={InputEvent} value={note.content} rows="" placeholder='Write a Note...'></textarea>
+                   
+                    {
+                        expand ?
+                            <ZoomInMapRoundedIcon className='abc' onClick={abcd} /> : <ZoomOutMapRoundedIcon className='abc' onClick={abc} />}
+                  {
+                    
+                    expand ?  <Button onClick={addEvent}>
+                        <AddIcon className='plus_sign' />
+                    </Button> : null
+                  } 
+                  
+
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default CreateNote
